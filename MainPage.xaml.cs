4559ca5bbde2e5f844bbd77cedae71394dfe3a49
Steve Sutton <steven.p.sutton@gmail.com>
2020-10-13 08:20:42 +0100

    first cut

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -26,5 +26,15 @@ namespace UWP
         {
             this.InitializeComponent();
         }
+
+        public async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MediaElement mediaElement = new MediaElement();
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await 
+                synth.SynthesizeTextToStreamAsync("Hello World");
+            mediaElement.SetSource(stream, stream.ContentType);
+            mediaElement.Play();
+        }
     }
 }
