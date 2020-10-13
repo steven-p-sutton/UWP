ac77666bfb3a4eacb68f630f8b288275a71252aa
Steve Sutton <steven.p.sutton@gmail.com>
2020-10-13 08:21:00 +0100

    First cut

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -31,8 +31,7 @@ namespace UWP
         {
             MediaElement mediaElement = new MediaElement();
             var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
-            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await 
-                synth.SynthesizeTextToStreamAsync("Hello World");
+            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync("Hello World");
             mediaElement.SetSource(stream, stream.ContentType);
             mediaElement.Play();
         }
