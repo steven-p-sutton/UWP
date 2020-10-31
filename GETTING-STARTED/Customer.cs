34b49c29f0e5307f7b8d641de7e297b1f7c832c8
steven-p-sutton <steven.p.sutton@gmail.com>
2020-10-31 20:19:53 +0000

    Edit your Customers through the UI

--- GETTING-STARTED/Customer.cs
+++ GETTING-STARTED/Customer.cs
@@ -1,13 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace GETTING_STARTED
 {
-    public class Customer
+    public class Customer : INotifyPropertyChanged
     {
-        public string Name { get; set; }
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
