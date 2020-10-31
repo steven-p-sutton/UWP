898a13c1cbc6f8f6b1793ea52612c8d5804abc32
steven-p-sutton <steven.p.sutton@gmail.com>
2020-10-31 21:56:07 +0000

    Tried Dynamic Page Content (reset back to static with comments

--- GETTING-STARTED/MainPage.xaml.cs
+++ GETTING-STARTED/MainPage.xaml.cs
@@ -31,10 +31,23 @@ namespace GETTING_STARTED
         {
             this.InitializeComponent();
 
+            // FIXED PAGE CONTENT
+            // https://docs.microsoft.com/en-us/windows/uwp/get-started/display-customers-in-list-learning-track
+
             // Add some customers
             this.Customers.Add(new Customer() { Name = "NAME1" });
             this.Customers.Add(new Customer() { Name = "NAME2" });
             this.Customers.Add(new Customer() { Name = "NAME3" });
+
+            // DYNAMIC PAGE CONTENT 
+            // https://docs.microsoft.com/en-us/windows/uwp/design/controls-and-patterns/listview-and-gridview
+
+            // Create a new ListView (or GridView) for the UI, add content by setting ItemsSource
+            // ListView ContactsLV = new ListView();
+            // ContactsLV.ItemsSource = Customers;
+
+            // Add the ListView to a parent container in the visual tree (that you created in the corresponding XAML file)
+            //Top.Children.Add(ContactsLV);
         }
     }
 }
