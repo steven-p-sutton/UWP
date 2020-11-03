02a716b7383eb7639a642bb05f3909537e7519e9
steven-p-sutton <steven.p.sutton@gmail.com>
2020-11-03 20:33:26 +0000

    working on save/get settings

--- GETTING-STARTED/MainPage.xaml.cs
+++ GETTING-STARTED/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -26,6 +27,11 @@ namespace GETTING_STARTED
         // https://docs.microsoft.com/en-us/windows/uwp/get-started/display-customers-in-list-learning-track
 
         public ObservableCollection<Customer> Customers { get; } = new ObservableCollection<Customer>();
+
+        // XMAL binds to these
+        string _AssociateName = string.Empty;
+        string _TargetInstallDate = string.Empty;
+        string _InstallTime = string.Empty;
 
         public MainPage()
         {
@@ -65,6 +71,32 @@ namespace GETTING_STARTED
             ////{
             ////    NameStack.Children[i].AccessKey.ToString();
             ////}
+
+            // Read settings 
+            GetSettings();
         }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveSettings();
+        }
+
+        private void SaveSettings()
+        {
+            // Save a setting locally on the device
+            ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            localSettings.Values["AssociateName"] = _AssociateName;
+        }
+        private void GetSettings()
+        {
+            // load a setting that is local to the device
+            ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            _AssociateName = localSettings.Values["AssociateName"] as string;
+            
+            //_AssociateName = "AssociateName";
+            _TargetInstallDate = "TargetInstallDate";
+            _InstallTime = "InstallTime";
+        }
+
     }
 }
