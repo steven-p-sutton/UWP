d14a52adfb2e281804be101831aaffde0302c647
steven-p-sutton <steven.p.sutton@gmail.com>
2020-11-06 10:21:10 +0000

    Create a "Hello, World!" app (XAML) - button & voice synthesiser

--- HELLOWORLD-2/MainPage.xaml.cs
+++ HELLOWORLD-2/MainPage.xaml.cs
@@ -26,5 +26,14 @@ namespace HELLOWORLD_2
         {
             this.InitializeComponent();
         }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            MediaElement mediaElement = new MediaElement();
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync("Hello, World!");
+            mediaElement.SetSource(stream, stream.ContentType);
+            mediaElement.Play();
+        }
     }
 }
